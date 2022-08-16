6346d78bcda12a2283fc1794473d978d872947d0
Goik <sbabinski@outlook.com>
2022-08-15 22:31:10 -0300

    Develop BST skeleton
    Write Tree and Node classes.
    Write the principal functions for the tree class.

---
+++ script.js
@@ -0,0 +1,41 @@
+class Node{
+    data;
+    left;
+    right;
+}
+
+class Tree{
+    root = buildTree();
+
+    buildTree(array){
+
+    }
+
+    insertNode(){
+
+    }
+
+    deleteNode(){
+
+    }
+
+    find(){
+
+    }
+
+    levelOrder(callback){
+
+    }
+
+    inOrder(){}
+    preOrder(){}
+    postOrder(){}
+
+    height(node){}
+
+    depth(node){}
+
+    isBalanced(){}
+
+    rebalance(){}
+}
